5d6c6625b05121f03c6fd5999ae8a413fecac2ac
omkarkshirsagar-7 <omkarkshirsagar66@gmail.com>
2021-04-23 18:25:35 +0530

    AddNums

---
+++ addnums.cpp
@@ -0,0 +1,13 @@
+//addition of given nos.
+
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c=0;
+    cin>>a;
+    cin>>b;
+    c=a+b;  
+    cout<<c;
+    return 0;
+}
