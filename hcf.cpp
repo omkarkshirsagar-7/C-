1a6cb91a074e60d461f0d5b0e392d003dae353f8
omkarkshirsagar-7 <omkarkshirsagar66@gmail.com>
2021-05-01 19:05:00 +0530

    HCF

---
+++ hcf.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a;
+    cin >> b;
+    while (a != b)
+    {
+        if (a > b)
+        {
+            a = a - b;
+        }
+        else
+        {
+            b = b - a;
+        }
+    }
+
+    cout << a << "\n";
+    return 0;
+}
